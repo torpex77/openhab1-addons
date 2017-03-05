2d52bdcf090d59f8ebf8d5cab047e5b4ebde67ab
Doug Dicks <doug@ten-13.org>
2017-03-04 21:28:42 -0500

    Add methods that don't have Pair as an argument so they can be called from Groovy rules. The Pair class was causing the rules to fail to compile.

--- bundles/action/org.openhab.action.mios/src/main/java/org/openhab/action/mios/internal/MiosAction.java
+++ bundles/action/org.openhab.action.mios/src/main/java/org/openhab/action/mios/internal/MiosAction.java
@@ -8,14 +8,15 @@
  */
 package org.openhab.action.mios.internal;
 
+import java.lang.reflect.Method;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import org.eclipse.xtext.xbase.lib.Pair;
 import org.openhab.binding.mios.MiosActionProvider;
-import org.openhab.core.items.Item;
 import org.openhab.core.scriptengine.action.ActionDoc;
 import org.openhab.core.scriptengine.action.ParamDoc;
 import org.slf4j.Logger;
@@ -23,100 +24,218 @@ import org.slf4j.LoggerFactory;
 
 /**
  * This class provides static methods, for invocation of MiOS Action and Scene requests, for use in automation rules.
- * 
+ *
  * @author Mark Clark
  * @since 1.7.0
  */
 public class MiosAction {
 
-	private static final Logger logger = LoggerFactory.getLogger(MiosAction.class);
+    private static final Logger logger = LoggerFactory.getLogger(MiosAction.class);
 
-	/**
-	 * Sends an Action invocation to a Device at a MiOS Unit, without parameters.
-	 */
-	@ActionDoc(text = "Sends an Action invocation to a Device at a MiOS Unit, without parameters.")
-	public static boolean sendMiosAction(
-			@ParamDoc(name = "item", text = "The Item used to determine the MiOS Unit Address information for sending the Action call.") Item item,
-			@ParamDoc(name = "action", text = "The Action string to be remotely invoked on the MiOS Unit.") String actionName) {
+    /**
+     * Sends an Action invocation to a Device at a MiOS Unit, without parameters.
+     */
+    @ActionDoc(text = "Sends an Action invocation to a Device at a MiOS Unit, without parameters.")
+    public static boolean sendMiosAction(
+            @ParamDoc(name = "item", text = "The Item used to determine the MiOS Unit Address information for sending the Action call.") Object item,
+            @ParamDoc(name = "action", text = "The Action string to be remotely invoked on the MiOS Unit.") String actionName) {
 
-		return sendMiosActionInternal(item.getName(), actionName, null);
-	}
+        return sendMiosActionInternal(item, actionName, (List<Pair>) null);
+    }
 
-	/**
-	 * Sends an Action invocation to a Device at a MiOS Unit, with parameters.
-	 */
-	@ActionDoc(text = "Sends an Action invocation to a Device at a MiOS Unit, with parameters.")
-	public static boolean sendMiosAction(
-			@ParamDoc(name = "item", text = "The Item used to determine the MiOS Unit Address information for sending the Action call.") Item item,
-			@ParamDoc(name = "actionName", text = "The Action string to be remotely invoked on the MiOS Unit.") String actionName,
-			@ParamDoc(name = "params", text = "The list of Action Parameters.") List<Pair> params) {
+    /**
+     * Sends an Action invocation to a Device at a MiOS Unit, without parameters.
+     */
+    @ActionDoc(text = "Sends an Action invocation to a Device at a MiOS Unit, without parameters.")
+    public static boolean sendMiosAction(
+            @ParamDoc(name = "item", text = "The Item used to determine the MiOS Unit Address information for sending the Action call.") String itemName,
+            @ParamDoc(name = "action", text = "The Action string to be remotely invoked on the MiOS Unit.") String actionName) {
 
-		return sendMiosActionInternal(item.getName(), actionName, params);
-	}
+        return sendMiosActionInternal(itemName, actionName, (List<Pair>) null);
+    }
 
-	/**
-	 * Sends a Scene invocation to a MiOS Unit.
-	 */
-	@ActionDoc(text = "Sends a Scene invocation to a MiOS Unit.")
-	public static boolean sendMiosScene(
-			@ParamDoc(name = "item", text = "The Item used to determine the MiOS Unit Address information for sending the Action call.") Item item) {
+    /**
+     * Sends an Action invocation to a Device at a MiOS Unit, with parameters.
+     */
+    @ActionDoc(text = "Sends an Action invocation to a Device at a MiOS Unit, with parameters.")
+    public static boolean sendMiosAction(
+            @ParamDoc(name = "item", text = "The Item used to determine the MiOS Unit Address information for sending the Action call.") Object item,
+            @ParamDoc(name = "actionName", text = "The Action string to be remotely invoked on the MiOS Unit.") String actionName,
+            @ParamDoc(name = "params", text = "The list of Action Parameters.") List<Pair> params) {
 
-		return sendMiosSceneInternal(item.getName());
-	}
+        return sendMiosActionInternal(item, actionName, params);
+    }
 
-	private static MiosActionProvider getActionProviderInternal(String itemName) throws Exception {
-		MiosActionService service = MiosActionService.getMiosActionService();
-		if (service == null) {
-			throw new Exception(String.format("MiOS Service is not configured, Action for Item %1$s not queued.",
-					itemName));
-		}
+    /**
+     * Sends an Action invocation to a Device at a MiOS Unit, with parameters.
+     */
+    @ActionDoc(text = "Sends an Action invocation to a Device at a MiOS Unit, with parameters.")
+    public static boolean sendMiosAction(
+            @ParamDoc(name = "item", text = "The Item used to determine the MiOS Unit Address information for sending the Action call.") Object item,
+            @ParamDoc(name = "actionName", text = "The Action string to be remotely invoked on the MiOS Unit.") String actionName,
+            @ParamDoc(name = "params", text = "The list of Action Parameters.") Map<String, Object> params) {
 
-		MiosActionProvider actionProvider = service.getMiosActionProvider();
-		if (actionProvider == null) {
-			throw new Exception(String.format(
-					"MiOS Action Provider is not configured, Action for Item %1$s not queued.", itemName));
-		}
+        return sendMiosActionInternal(item, actionName, params);
+    }
 
-		return actionProvider;
-	}
+    /**
+     * Sends an Action invocation to a Device at a MiOS Unit, with parameters.
+     */
+    @ActionDoc(text = "Sends an Action invocation to a Device at a MiOS Unit, with parameters.")
+    public static boolean sendMiosAction(
+            @ParamDoc(name = "item", text = "The Item used to determine the MiOS Unit Address information for sending the Action call.") String itemName,
+            @ParamDoc(name = "actionName", text = "The Action string to be remotely invoked on the MiOS Unit.") String actionName,
+            @ParamDoc(name = "params", text = "The list of Action Parameters.") List<Pair> params) {
 
-	private static boolean sendMiosActionInternal(String itemName, String actionName, List<Pair> params) {
-		try {
-			logger.debug("Attempting to invoke MiOS Action {} using Item {} and {} Parameters", new Object[] {
-					actionName, itemName, (params == null) ? 0 : params.size() });
-			ArrayList<Entry<String, Object>> paramList;
+        return sendMiosActionInternal(itemName, actionName, params);
+    }
 
-			// Convert from XText to the form needed to invoke MiOS.
-			if (params != null) {
-				paramList = new ArrayList<Entry<String, Object>>(params.size());
+    /**
+     * Sends an Action invocation to a Device at a MiOS Unit, with parameters.
+     */
+    @ActionDoc(text = "Sends an Action invocation to a Device at a MiOS Unit, with parameters.")
+    public static boolean sendMiosAction(
+            @ParamDoc(name = "item", text = "The Item used to determine the MiOS Unit Address information for sending the Action call.") String itemName,
+            @ParamDoc(name = "actionName", text = "The Action string to be remotely invoked on the MiOS Unit.") String actionName,
+            @ParamDoc(name = "params", text = "The list of Action Parameters.") Map<String, Object> params) {
 
-				for (Pair<String, String> p : params) {
-					logger.trace("Type of parameter key={} value={}", p.getKey(), p.getValue());
-					paramList.add(new AbstractMap.SimpleImmutableEntry<String, Object>(p.getKey(), p.getValue()));
-				}
-			} else {
-				paramList = null;
-			}
+        return sendMiosActionInternal(itemName, actionName, params);
+    }
 
-			MiosActionProvider actionProvider = getActionProviderInternal(itemName);
+    /**
+     * Sends a Scene invocation to a MiOS Unit.
+     */
+    @ActionDoc(text = "Sends a Scene invocation to a MiOS Unit.")
+    public static boolean sendMiosScene(
+            @ParamDoc(name = "item", text = "The Item used to determine the MiOS Unit Address information for sending the Action call.") Object item) {
 
-			return actionProvider.invokeMiosAction(itemName, actionName, paramList);
-		} catch (Exception ex) {
-			logger.error(ex.getMessage(), ex);
-			return false;
-		}
-	}
+        return sendMiosSceneInternal(item);
+    }
 
-	private static boolean sendMiosSceneInternal(String itemName) {
-		try {
-			logger.debug("Attempting to invoke MiOS Scene using Item {}", itemName);
+    /**
+     * Sends a Scene invocation to a MiOS Unit.
+     */
+    @ActionDoc(text = "Sends a Scene invocation to a MiOS Unit.")
+    public static boolean sendMiosScene(
+            @ParamDoc(name = "item", text = "The Item used to determine the MiOS Unit Address information for sending the Action call.") String itemName) {
 
-			MiosActionProvider actionProvider = getActionProviderInternal(itemName);
+        return sendMiosSceneInternal(itemName);
+    }
 
-			return actionProvider.invokeMiosScene(itemName);
-		} catch (Exception ex) {
-			logger.error(ex.getMessage(), ex);
-			return false;
-		}
-	}
+    private static MiosActionProvider getActionProviderInternal(String itemName) throws Exception {
+        MiosActionService service = MiosActionService.getMiosActionService();
+        if (service == null) {
+            throw new Exception(
+                    String.format("MiOS Service is not configured, Action for Item %1$s not queued.", itemName));
+        }
+
+        MiosActionProvider actionProvider = service.getMiosActionProvider();
+        if (actionProvider == null) {
+            throw new Exception(String
+                    .format("MiOS Action Provider is not configured, Action for Item %1$s not queued.", itemName));
+        }
+
+        return actionProvider;
+    }
+
+    private static String getName(Object item) throws Exception {
+        Method method = item.getClass().getMethod("getName");
+        return (String) method.invoke(item);
+    }
+
+    private static boolean sendMiosActionInternal(Object item, String actionName, List<Pair> params) {
+        try {
+            // Both OH 1 and ESH have a "getName" method.
+            return sendMiosActionInternal(getName(item), actionName, params);
+        } catch (Exception e) {
+            logger.error("An unexpected error occurred using sendMiosAction: {}", e);
+            return false;
+        }
+    }
+
+    private static boolean sendMiosActionInternal(Object item, String actionName, Map<String, Object> params) {
+        try {
+            // Both OH 1 and ESH have a "getName" method.
+            return sendMiosActionInternal(getName(item), actionName, params);
+        } catch (Exception e) {
+            logger.error("An unexpected error occurred using sendMiosAction: {}", e);
+            return false;
+        }
+    }
+
+    private static boolean sendMiosActionInternal(String itemName, String actionName, List<Pair> params) {
+        try {
+            logger.debug("Attempting to invoke MiOS Action {} using Item {} and {} Parameters",
+                    new Object[] { actionName, itemName, (params == null) ? 0 : params.size() });
+            ArrayList<Entry<String, Object>> paramList;
+
+            // Convert from XText to the form needed to invoke MiOS.
+            if (params != null) {
+                paramList = new ArrayList<Entry<String, Object>>(params.size());
+
+                for (Pair<String, String> p : params) {
+                    logger.trace("Type of parameter key={} value={}", p.getKey(), p.getValue());
+                    paramList.add(new AbstractMap.SimpleImmutableEntry<String, Object>(p.getKey(), p.getValue()));
+                }
+            } else {
+                paramList = null;
+            }
+
+            MiosActionProvider actionProvider = getActionProviderInternal(itemName);
+
+            return actionProvider.invokeMiosAction(itemName, actionName, paramList);
+        } catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+            return false;
+        }
+    }
+
+    private static boolean sendMiosActionInternal(String itemName, String actionName, Map<String, Object> params) {
+        try {
+            logger.debug("Attempting to invoke MiOS Action {} using Item {} and {} Parameters",
+                    new Object[] { actionName, itemName, (params == null) ? 0 : params.size() });
+            ArrayList<Entry<String, Object>> paramList;
+
+            // Convert from XText to the form needed to invoke MiOS.
+            if (params != null) {
+                paramList = new ArrayList<Entry<String, Object>>(params.size());
+                for (Map.Entry<String, Object> p : params.entrySet()) {
+                    logger.trace("Type of parameter key={} value={}", p.getKey(), p.getValue());
+                    paramList.add(new AbstractMap.SimpleImmutableEntry<String, Object>(p.getKey(), p.getValue()));
+                }
+            } else {
+                paramList = null;
+            }
+
+            MiosActionProvider actionProvider = getActionProviderInternal(itemName);
+
+            return actionProvider.invokeMiosAction(itemName, actionName, paramList);
+        } catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+            return false;
+        }
+    }
+
+    private static boolean sendMiosSceneInternal(Object item) {
+        try {
+            // Both OH 1 and ESH have a "getName" method.
+            return sendMiosSceneInternal(getName(item));
+        } catch (Exception e) {
+            logger.error("An unexpected error occurred using sendMiosScene: {}", e);
+            return false;
+        }
+    }
+
+    private static boolean sendMiosSceneInternal(String itemName) {
+        try {
+            logger.debug("Attempting to invoke MiOS Scene using Item {}", itemName);
+
+            MiosActionProvider actionProvider = getActionProviderInternal(itemName);
+
+            return actionProvider.invokeMiosScene(itemName);
+        } catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+            return false;
+        }
+    }
 }
